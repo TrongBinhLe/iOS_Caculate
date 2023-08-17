2a059e8eb321acddfdc7ce560a52120d1e0301dc
TrongBinhLe <ltbinhbk2010@gmail.com>
2023-08-17 12:50:00 +0700

    Section 3:Compute result in viewmoel

--- Caculate/ViewModel/CalculatorVM.swift
+++ Caculate/ViewModel/CalculatorVM.swift
@@ -24,7 +24,17 @@ class CalculatorVM {
     
     func transform(input: Input) -> Output {
         
-        let result = Result(amountPerPerson: 500, totalBill: 299, totalTip: 200)
+        let updateViewPulisher = Publishers.CombineLatest3(
+            input.billPublisher,
+            input.tipPublisher,
+            input.splitPublisher).flatMap {[unowned self] (bill, tip, split) in
+            let totalTip = getTipAmount(bill: bill, tip: tip)
+            let totalBill = bill + totalTip
+            let amountPerPerson = totalTip / Double(split)
+            let result = Result(amountPerPerson: amountPerPerson, totalBill: totalBill, totalTip: totalTip)
+            return Just(result)
+            }.eraseToAnyPublisher()
+    
         input.tipPublisher.sink{ tip in
             print("DEBUG: >> tip : \(tip)")
         }.store(in: &cancellables)
@@ -33,6 +43,21 @@ class CalculatorVM {
             print("DEBUG: >> split : \(tip)")
         }.store(in: &cancellables)
         
-        return Output(updateViewPublisher: Just(result).eraseToAnyPublisher())
+        return Output(updateViewPublisher: updateViewPulisher)
+    }
+    
+    private func getTipAmount(bill: Double, tip: Tip) -> Double {
+        switch tip {
+        case .none:
+            return 0
+        case .tenPercent:
+            return bill * 0.1
+        case .fiftenPercent:
+            return bill * 0.15
+        case .twentyPercent:
+            return bill * 0.2
+        case .custom(let value):
+            return Double(value)
+        }
     }
 }
